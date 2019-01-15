f1f4ecf465c70576fb41df982388d0ca678610b7
Matthew Tekampe <TekampML19@pwcs-edu.org>
2019-01-15 13:10:27 -0500

    helllloooo

--- manipulation.hs
+++ manipulation.hs
@@ -1,9 +1,11 @@
 firstMethod x xs = if x `mod` 2 == 0 then xs else reverse xs
-
-
 methodTwo xs x = if x `mod` 2 == 0 then xs else reverse xs
-
-
-
 blah xs = [methodTwo (xs!!i) i|i <- [0..(length xs - 1)]]
+
+
+
+sum a b = a + b
+
+
+
 
